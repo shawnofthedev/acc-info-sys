0b72de3a14918e3ef1827f4677bc362b453a4a11
Shawn <svotaw81@gmail.com>
2019-10-14 12:05:44 -0400

    Add project files.

---
+++ acc-info-sys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace acc_info_sys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
